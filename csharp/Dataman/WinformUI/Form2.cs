522e3e5533b6af7dcb5b3ea3a12edd300c032591
Joseph McCollum <mccolluj7506@student.faytechcc.edu>
2023-11-13 21:30:35 -0500

    Changed button names added functionality to some buttons/ Added loading Screen

---
+++ csharp/Dataman/WinformUI/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformUI
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            progressBar1.Increment(2);
+            if (progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+        }
+    }
+}
